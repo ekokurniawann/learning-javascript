8b3194aa3832304b266bdfc0c14352afe1f91b13
ekokurniawann <ekokurniawaann@gmail.com>
2024-07-31 00:45:32 +0700

    Comments

---
+++ comments/main.js
@@ -0,0 +1,6 @@
+// Ini merupakan komentar satu baris, kode dibawah tidak akan dijalankan 
+// console.log("Halo!");
+
+/* Ini merupakan komentar dengan lebih dari satu baris Teks apapun yang berada
+ disini akan dijadikan komentar. Ketika menggunakan ini, jangan lupa untuk menutupnya.
+ */
