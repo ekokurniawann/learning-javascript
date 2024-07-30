8fb765e5e0c87dabb86474da031aca9b48abc661
ekokurniawann <ekokurniawaann@gmail.com>
2024-07-31 01:06:37 +0700

    Variable

---
+++ variable/main.js
@@ -0,0 +1,30 @@
+// Deklarasi variabel dengan let 
+let lastName;
+lastName = "Skywalker";
+
+console.log(lastName);
+
+// Deklarasi variabel dan inisialisasi 
+let firstName = "Roger";
+console.log(firstName);
+
+let lastName1 = "Skywalker1" // Assigment expression 
+
+/*
+let fullName = let lastName; // Error, karena let lastName adalah statement
+let fullName = (lastName = "Skywalker"); // Valid, karena (lastName = "Skywalker") adalah expression
+let fullName = "Luke" + "Skywalker"; // Valid, karena "Luke" + "Skywalker" adalah expression        
+ */
+
+/*
+Penamaan variabel yang valid: 
+let firstName;
+let _lastName;
+let catName;
+*/
+
+// Penggunaan const 
+const z = 100
+console.log(z);
+// z = 200 // error: TypeError: Assigment to constant variabel
+
