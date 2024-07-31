131d77c1df3950ee88203a9f880aeb18f6f91972
ekokurniawann <ekokurniawaann@gmail.com>
2024-07-31 23:40:01 +0700

    Loop

---
+++ Loop/main.js
@@ -0,0 +1,42 @@
+// For loop
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// For of loop 
+let myArray = ["Luke", "Han", "Chewbacca", "Leia"];
+
+for (const arrayItem of myArray) {
+    console.log(arrayItem)
+}
+
+// While   
+let i = 1;
+
+while (i <= 100) {
+    console.log(i);
+    i++;
+}
+
+// do-while
+let i1 = 1;
+
+do {
+    console.log(i1);
+    i1++;
+} while (i1 <= 100);
+
+/*
+Infinite loops 
+case 1:
+let i2 = 1;
+
+while (i2 <= 5) {
+    console.log(i2);
+}
+
+case 2:
+for(let i = 1; i <= 5; i=1) {
+    console.log(i);
+}
+*/
