dcba10e38fc9e3a3f51b1f388dd5b9bc4bb03b1f
ekokurniawann <ekokurniawaann@gmail.com>
2024-08-01 02:55:52 +0700

    docs: add object initialization methods in JavaScript

--- object/main.js
+++ object/main.js
@@ -11,18 +11,22 @@ console.log(user1);
 };
  */
 
-// Contoh membuat object
+// Contoh membuat object (Literal object)
 const user = {
     firstName: "Luke",
     lastName: "Skywalker",
     age: 19,
     isJedi: true,
-    "home world": "Tattooine"
+    "home world": "Tattooine",
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
 };
 
 // Mengakses properti object : Untuk mengakses nilai dari properti object, kita dapat menggunakan dot operator (.) atau bracket notation ([]).
 // Dot operator 
 console.log(`Halo, nama saya ${user.firstName} ${user.lastName}`);
+console.log(user.getFullName());
 console.log(`Umur saya ${user.age} tahun`)
 
 // Bracket Notation digunakan ketika key memiliki spasi atau karakter khusus lainnya.
@@ -46,3 +50,116 @@ spaceship.class = "Light freighter";
 delete spaceship.manufacturer;
 
 console.log(spaceship);
+
+// Membuat object constructor 
+const user2 = new Object();
+user2.firstName = "Jane";
+user2.lastName = "Smith";
+user2.age = 30;
+
+console.log(user2.firstName)
+
+
+//// Fungsi konstruktor (ES5)
+//function User(firstName, lastName, age) {
+//    this.firstName = firstName;
+//    this.lastName = lastName;
+//    this.age = age;
+//}
+//
+//User.prototype.getFullName = function() {
+//    return `${this.firstName} ${this.lastName}`;
+//};
+//
+//const user3 = new User("Bob", "Brown", 32);
+//console.log(user3.getFullName()); // Output: Bob Brown
+
+
+// Class Syntax : menggunakan sintaks ES6 untuk mendefinisikan objek dan metode
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+const user4 = new User("Roger", "Danuarta", 32);
+console.log(user4.getFullName());
+
+
+// Method Object.create() : digunakan untuk membuat object baru dengan object yang ada sebagai prototipe
+const person = {
+    greet() {
+        return `Hello, my name is ${this.name}`;
+    }
+};
+
+const john = Object.create(person);
+john.name = "John";
+console.log(john.greet());
+
+
+// Factory functions 
+function createUser(firstName, lastName, age) {
+    return {
+        firstName,
+        lastName,
+        age,
+        getFullName() {
+            return `${this.firstName} ${this.lastName}`;
+        }
+    };
+
+}
+
+const user5 = createUser("Alice", "Smith", 30);
+console.log(user5.getFullName());
+
+
+// Object destructuring : untuk mengambil properti dari object
+const user6 = {
+    firstName: "Alice",
+    lastName: "Johnson",
+    age: 28
+};
+
+const { firstName, lastName } = user6;
+console.log(firstName);
+console.log(lastName);
+
+
+// Object spread and rest : Mnggunakan spread operator (...) untuk menyalin dan menggabungkan object
+const user7 = {
+    firstName: "Alice",
+    lastName: "Gusion",
+    age: 29
+};
+
+const userWithLocation = {
+    ...user7,
+    location: "New York"
+};
+
+console.log(userWithLocation);
+
+
+// Property shorthand : Menulis properti objek dengan nama variabel yang sama menggunakan shorthand
+const firstName1 = "Alice";
+const lastName1 = "Alucard";
+
+const user8 = { firstName1, lastName1, age: 28 };
+console.log(user8);
+
+
+// Computed property names : mengunkana nama properti yaang dinamis dalam objek 
+const key = "email";
+const user9 = {
+    [key]: "zilong@gmail.com"
+};
+
+console.log(user9.email);
