94ff97441f4b20c2043350bd94dac3e5dd6ed3db
ekokurniawann <ekokurniawaann@gmail.com>
2024-08-05 01:04:34 +0700

    docs(array): update documentation

--- array/main.js
+++ array/main.js
@@ -14,7 +14,7 @@ console.log(myArray[5]);
 console.log("Panjang nilai myArray adalah " + myArray.length + ".");
 
 // Menambahkan elemen 'JavaScript' di akhir array
-myArray.push('JavaScript'); // Menambahkan 'JavaScript' di akhir array
+myArray.push("JavaScript"); // Menambahkan 'JavaScript' di akhir array
 console.log(myArray);
 
 // Menghapus elemen terakhir
@@ -25,24 +25,20 @@ console.log(myArray);
 myArray.shift(); // Menghapus elemen pertama dari array
 console.log(myArray);
 
-
 // Menambahkan elemen 'Apple' di awal array
 myArray.unshift("Apple"); // Menambahkan 'Apple' di awal array
 console.log(myArray);
-
 
 // Menghapus elemen pada indeks ke-1
 delete myArray[1]; // Menghapus elemen pada indeks ke-1
 console.log(myArray); // Menghapus 1 elemen mulai dari indeks ke-2
 
-
 myArray.splice(2, 1); // Menghapus elemen pada indeks ke-2, yaitu elemen '22'
 console.log(myArray);
 
-
 // Menambahkan elemen 'February' pada indeks ke-1 tanpa menghapus elemen apapun
-const month = ['January', 'March', 'April', 'May'];
-console.log('before splice: ', month);
+const month = ["January", "March", "April", "May"];
+console.log("before splice: ", month);
 
-month.splice(1, 0, 'February'); // Menambahkan 'February' pada indeks ke-1
-console.log('after splice: ', month); 
+month.splice(1, 0, "February"); // Menambahkan 'February' pada indeks ke-1
+console.log("after splice: ", month);
